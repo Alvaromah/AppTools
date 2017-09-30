818fc326673f0b2cc5d0d06cc4b72626d60d2c3b
Alvaro Mateos <alvaroma@live.com>
2017-09-30 03:30:32 +0200

    Added Lorem tool.

---
+++ Win32Tools/Win32Tools/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using AppTools;
+
+namespace Win32Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoremSample();
+        }
+
+        private static void LoremSample()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Lorem.Generate(10));
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Lorem.Generate(40));
+            Console.WriteLine();
+            Console.WriteLine(Lorem.Generate(60));
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Lorem.Generate(10));
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(Lorem.Generate(50));
+            Console.WriteLine();
+            Console.WriteLine(Lorem.Generate(40));
+            Console.WriteLine();
+        }
+    }
+}
